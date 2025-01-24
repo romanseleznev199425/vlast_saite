1c17397f29f313b70766dab46cc35276ab204ecb
romanseleznev199425 <romanseleznev199425@gmail.com>
2025-01-24 16:15:42 +0300

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -66,4 +66,40 @@ var mySwiperBanners = new Swiper('.mySwiperBanners', {
     }
 });
 
+
+var mySwiperPhotogallery = new Swiper('.mySwiperPhotogallery', {
+    /// loop: true, // Включение циклического пролистывания
+   // autoplay: { // Автопрокрутка
+   //     delay: 5000,
+  //      disableOnInteraction: false,
+   //  },
+    spaceBetween: 66.6,
+    //slideWidth: 250,
+    navigation: {
+        nextEl: '.swiper-btn-next',
+        prevEl: '.swiper-btn-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    slidesPerView: 4, 
+    breakpoints: {
+        320: {
+            slidesPerView: 1.2,
+        },
+        768: {
+            slidesPerView: 2.5,
+        },
+        840: {
+            slidesPerView: 3,
+        },
+        980: {
+            slidesPerView: 4,
+        },
+        1024: {
+            slidesPerView: 4,
+        }
+    }
+});
 })
